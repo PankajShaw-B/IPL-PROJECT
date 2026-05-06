e92d3b3790ca45fc0571e28e9a82f2c6730d53ca
Pankaj Shaw <pankajshaw979@gmail.com>
2026-05-06 09:39:21 +0530

    Added function for number of matches played per year of all the years in IPL

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import analysis.Analysis;
+import java.util.List;
+import loader.CsvLoader;
+import model.Delivery;
+import model.Match;
+
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        List<Match>    matches    = CsvLoader.loadMatches("data/matches.csv");
+        List<Delivery> deliveries = CsvLoader.loadDeliveries("data/deliveries.csv");
+
+        System.out.println("Matches loaded:    " + matches.size());
+        System.out.println("Deliveries loaded: " + deliveries.size());
+        System.out.println();
+
+        Analysis analysis = new Analysis();
+
+        analysis.matchesPlayedPerYear(matches);
+        
+    }
+}
